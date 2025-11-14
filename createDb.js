73986717aaa83431073c2de6c7b3fcbc5619ee57
MariaAhle <mariaahle1@gmail.com>
2025-11-14 13:13:15 +0100

    rigtig rækkefølge nu

--- createDb.js
+++ createDb.js
@@ -1,26 +1,18 @@
 await db.query(`
-    drop table if exists transactions;
-    create table transactions (
-        hash            text,
-        block_id        integer references block (block_id),
-        transaction_id  integer primary key,
-    );
-`); 
-
-await db.query(`
-     create table block (
+    drop table if exists block;
+    create table block (
         hash       numeric,
         timestamp  timestamp,
         block_id   integer
-    );
+);
 `);
-
+        
 await db.query(`
-     create table price (
-        price       numeric,
-        valuta_id   integer, 
-        timestamp   timestamp  
-    );
+    drop table if exists address;
+    create table address (
+        address_id    integer primary key,
+        address       text
+);
 `);
 
 await db.query(`
@@ -28,32 +20,39 @@ await db.query(`
     create table valuta (
         value             numeric,
         valuta_id         integer primary key          
-    );
+);
 `);
 
 await db.query(`
-     drop table if exists transfer;
-     create table transfer (
+    drop table if exists transactions;
+    create table transactions (
+        hash            text,
+        block_id        integer references block (block_id),
+        transaction_id  integer primary key,
+);
+`); 
+
+await db.query(`
+    drop table if exists price; 
+    create table price (
+        price       numeric,
+        valuta_id   integer, 
+        timestamp   timestamp  
+);
+`);
+
+await db.query(`
+    drop table if exists transfer;
+    create table transfer (
         sender              integer references address (address_id),
         reciever            integer references address (address_id), 
         valuta_id           integer references valuta (valuta_id),
         transactions_id     integer references transaction (transaction_id),
         value               numeric
-    );
+);
 `);
+        
 
-await db.query(`
-    drop table if exists address;
-    create table address (
-        address_id    integer primary key,
-        address       text
-    );
-`);
 
-await db.query(`
-     create table block (
-        hash       numeric,
-        timestamp  timestamp,
-        block_id   integer
-    );
-`);
+
+await db.end();
