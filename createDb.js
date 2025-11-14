37cf1707cc4812368834973f0cc2f8d0a4382bb3
sebkahr <sebkahr@gmail.com>
2025-11-14 13:53:14 +0100

    lavet insert for valuta

--- createDb.js
+++ createDb.js
@@ -71,6 +71,15 @@ await db.query(`
 `);
         
 
+await db.query(`
+    insert into valuta
+    (valuta_id, value)
+    values
+    (0, null),
+    (1, null),
+    (2, null)
+);
 
 
 await db.end();
+
