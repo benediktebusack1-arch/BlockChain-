db248631a9f53cf9595464af1f1cba8da7b47f2f
XudayfeGuhad <Xudayfe1@outlook.dk>
2025-11-14 13:37:54 +0100

    har lavet server

--- createDb.js
+++ createDb.js
@@ -1,15 +1,19 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+import { upload } from 'pg-upload';
 
-
-
-
-
-
-
-
-
-
-
-
+dotenv.config();
+console.log('Connecting to database', process.env.PG_DATABASE);
+const db = new pg.Pool({
+    host:     process.env.PG_HOST,
+    port:     parseInt(process.env.PG_PORT),
+    database: process.env.PG_DATABASE,
+    user:     process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    ssl:      { rejectUnauthorized: false },
+});
+const dbResult = await db.query('select now()');
+console.log('Database connection established on', dbResult.rows[0].now);
 
 
 await db.query(`
